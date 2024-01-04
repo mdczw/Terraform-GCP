32af69bb0d497f6413dd77bae5369ee6ba23b600
Rita <mdyczewska@griddynamics.com>
2024-01-04 11:06:42 +0100

    Add static-ip-address for lb

--- modules/load_balancer/main.tf
+++ modules/load_balancer/main.tf
@@ -1,22 +1,26 @@
+resource "google_compute_global_address" "default" {
+  name         = "xlb-static-ip-address"
+  address_type = "EXTERNAL"
+}
 resource "google_compute_global_forwarding_rule" "default" {
-  name                  = "l7-xlb-forwarding-rule"
+  name                  = "xlb-forwarding-rule"
   ip_protocol           = "TCP"
   load_balancing_scheme = "EXTERNAL"
   port_range            = "80"
   target                = google_compute_target_http_proxy.default.id
-#  source_ip_ranges      = ["89.64.40.117"]
+  ip_address            = google_compute_global_address.default.id
 }
 resource "google_compute_target_http_proxy" "default" {
-  name = "test-proxy-health-check"
+  name    = "test-proxy-health-check"
   url_map = google_compute_url_map.default.id
 }
 resource "google_compute_url_map" "default" {
-  name            = "l7-xlb-url-map"
+  name            = "xlb-url-map"
   default_service = google_compute_backend_service.default.id
 }
 
 resource "google_compute_backend_service" "default" {
-  name                  = "l7-xlb-backend-service"
+  name                  = "xlb-backend-service"
   protocol              = "HTTP"
   port_name             = "http"
   load_balancing_scheme = "EXTERNAL"
@@ -29,7 +33,7 @@ resource "google_compute_backend_service" "default" {
   }
 }
 resource "google_compute_health_check" "default" {
-  name               = "tcp-proxy-health-check"
+  name = "tcp-proxy-health-check"
   http_health_check {
     port_specification = "USE_SERVING_PORT"
   }
